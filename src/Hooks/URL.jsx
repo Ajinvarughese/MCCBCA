600019e50e70ebb3e9f488f2e4490188ab1070a3
Ajin Varughese <ajinvarughese91@gmail.com>
2024-11-24 18:41:50 +0530

    update

--- src/Hooks/URL.jsx
+++ src/Hooks/URL.jsx
@@ -1,6 +1,6 @@
 const URL = () => {
     return {
-        api: "https://api-mccbca-production.up.railway.app/api/"
+        api: "https://desirable-charm-production.up.railway.app/api/"
     }    
 }
 
